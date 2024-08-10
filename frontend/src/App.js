b114c6bef6a6dc0260d4c7cb08385622fe0fd090
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-08-10 11:30:46 -0400

    updated flask and react setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,51 @@
-import React, { useState, useEffect } from 'react'
 
+
+// Filename - App.js
+ 
+// Importing modules
+import React, { useState, useEffect } from "react";
+import "./App.css";
+ 
 function App() {
-  const [data, setData] = useState([{}])
-
-  // View on console
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-  return (
-    <div>
-      {(typeof data.members === "undefined") ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
-      )}
-    </div>
-  )
+    // usestate for setting a javascript
+    // object for storing and using data
+    const [data, setdata] = useState({
+        name: "",
+        age: 0,
+        date: "",
+        programming: "",
+    });
+ 
+    // Using useEffect for single rendering
+    useEffect(() => {
+        // Using fetch to fetch the api from 
+        // flask server it will be redirected to proxy
+        fetch("/data").then((res) =>
+            res.json().then((data) => {
+                // Setting a data from api
+                setdata({
+                    name: data.Name,
+                    age: data.Age,
+                    date: data.Date,
+                    programming: data.programming,
+                });
+            })
+        );
+    }, []);
+ 
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>React and flask</h1>
+                {/* Calling a data from setdata for showing */}
+                <p>{data.name}</p>
+                <p>{data.age}</p>
+                <p>{data.date}</p>
+                <p>{data.programming}</p>
+ 
+            </header>
+        </div>
+    );
 }
-
-export default App
+ 
+export default App;
